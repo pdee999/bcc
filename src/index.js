6b7ee2a0f18019ecbff276ebe05fbf63d92a24a2
patrick.rittenhouse <patrick.rittenhouse@beaconfire-red.com>
2017-03-04 19:40:52 -0500

    adding sass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//REQUIRE SASS
+require('../sass/main.scss');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: [
-    `./src/index.js`
+    './src/index.js'
   ],
   output: {
     path: __dirname,
@@ -8,14 +10,50 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders:[{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        test: /.*\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader?sourceMap', 'ruby-sass?sourceMap']
+        })
       }
-    }]
+    ],
+    loaders:[
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.(jpe?g|gif|png)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[name].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false&name=/[name].[ext]'
+        ]
+      },
+      {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?|#[#?a-z]+)?(\?|#[#?0-9]+)?(#[a-z]+)?(#[0-9]+)?/,
+        loader: 'file-loader?name=/[name].[ext]'
+      },
+      {
+        test: /.*\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader?sourceMap!ruby-sass?sourceMap'),
+        fallbackLoader: 'style-loader!css-loader?sourceMap!postcss-loader?sourceMap!ruby-sass?sourceMap'
+      }
+    ]
   },
+  postcss: function() {
+    return [
+      require('autoprefixer')
+    ];
+  },
+  plugins: [
+    new ExtractTextPlugin("../css/styles.main.css")
+  ],
+  devtool: 'source-map',
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
